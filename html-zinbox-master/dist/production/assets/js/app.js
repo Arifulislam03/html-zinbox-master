3422c0edc74e43b91f3ee69664a34982ce61e7cd
Arifulislam03 <75440947+Arifulislam03@users.noreply.github.com>
2022-02-17 06:39:21 +0600

    first commit

---
+++ html-zinbox-master/dist/production/assets/js/app.js
@@ -0,0 +1,106 @@
+(function ($) {
+	"use strict";
+
+// /* meanmenu */
+
+	$('#mobile-menu').meanmenu({
+		
+		meanScreenWidth: '991',
+		meanMenuContainer:'.mobile-menu',
+		
+	});
+	
+	
+/* post-carousel */	
+$('.post-carousel').owlCarousel({
+	dots:true,
+	nav:false,
+	
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+
+	
+	
+    $('.infinite-scroll').infiniteScroll({
+        path: function path() {
+            var pageNumber = this.loadCount + 1;
+            return 'page' + pageNumber + '.html';
+        },
+        append: '.post-load',
+        button: '.loadmore-button',
+        scrollThreshold: false,
+    });	
+
+ 
+ 
+  	
+
+
+/*  aos js*/
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+
+/* ScrollUp */
+    jQuery.scrollUp({
+    scrollName: 'scrollUp', // Element ID
+    topDistance: '300', // Distance from top before showing element (px)
+    topSpeed: 4000, // Speed back to top (ms)
+    animation: 'fade', // Fade, slide, none
+    animationInSpeed: 1000, // Animation in speed (ms)
+    animationOutSpeed: 1000, // Animation out speed (ms)
+    scrollText: '↑', // Text for element
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+  });
+  
+ 
+
+/* Tooltip */
+  $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+}(jQuery));
+
+ 
+$(window).on('load',function(){
+
+/*  Preloader js*/
+        var preLoder = $("#preloader");
+        preLoder.fadeOut(1000);
+       
+    });    
+		
+       
